eac6b828e89d3a24ac38bc9414eb424a3425fd56
Ganesh Agrawal <118963973+Ganesh-Agrawal@users.noreply.github.com>
2023-04-23 20:59:52 +0530

    add skill education and achievmenets

--- my-resume-builder/src/components/CreateNewResume.js
+++ my-resume-builder/src/components/CreateNewResume.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 export default function CreateNewResume() {
 
@@ -6,23 +6,23 @@ export default function CreateNewResume() {
     state: "",
     city: ""
   });
-    const handleStateChange = (event) => {
-      const { value } = event.target;
-      setFormData({
-        ...formData,
-        state: value,
-        city: "" // Reset city value when state changes
-      });
-    };
-  
-    const handleCityChange = (event) => {
-      const { value } = event.target;
-      setFormData({
-        ...formData,
-        city: value
-      });
-    };
-  
+  const handleStateChange = (event) => {
+    const { value } = event.target;
+    setFormData({
+      ...formData,
+      state: value,
+      city: "" // Reset city value when state changes
+    });
+  };
+
+  const handleCityChange = (event) => {
+    const { value } = event.target;
+    setFormData({
+      ...formData,
+      city: value
+    });
+  };
+
   return (
     <>
       <div className="container mt-3 ml-auto card-body my-3 card col-md-6 row justify-content-center">
@@ -77,13 +77,73 @@ export default function CreateNewResume() {
               <label className="input-group-text" htmlFor="pincode">Postal Code</label>
               <input type="text" className="form-control" id="pincode" placeholder="Enter You Area Pincode" />
             </div>
+            <div class="input-group mb-3">
+              <div className="input-group-prepend custom-file">
+                <span className="input-group-text">Upload Profile</span>
+                <input type="file" className="my-2 custom-file-input" id="inputGroupFile01" />
+              </div>
+            </div>
             <div className=" my-2">
-              <button type="submit" id="signup" className="btn btn-primary">Save and Next</button>
+              <button type="submit" id="nextToeducation" className="btn btn-primary">Save and Next</button>
             </div>
           </form>
-
         </div>
       </div>
+      <h1>second fiels</h1>
+      <div className="container mt-3 ml-auto card-body my-3 card col-md-6 row justify-content-center">
+        <div className="card-header my-2">Educational Details</div>
+        <div>
+          <form>
+            <div className="form-group input-group my-3">
+              <label className="input-group-text" htmlFor="institutename">Organigation Name</label>
+              <input type="text" className="form-control" id="institutename" placeholder="Enter Institute Name" />
+            </div>
+            <div className="form-group input-group my-3">
+              <label className="input-group-text" htmlFor="universityname">Univercity Name</label>
+              <input type="text" className="form-control" id="universityname" placeholder="Enter University Name" />
+            </div>
+            <div className="form-group input-group my-3">
+              <label className="input-group-text" htmlFor="degree">Degree </label>
+              <input type="text" className="form-control" id="degree" placeholder="Enter Your Degree" />
+            </div>
+            <div className="form-group input-group my-3">
+              <label className="input-group-text" htmlFor="endyear">End Year</label>
+              <input type="date" className="form-control mx-1" id="endyear" placeholder="Select End Year" />
+            </div>
+            <div className=" my-2 mx-2">
+              <button type="submit" id="addeducation" className="mx-2 btn btn-info">Add Education</button>
+              <button type="submit" id="nextToskill" className="mx-2 btn btn-primary">Save and Next</button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <h1>third fiels</h1>
+      <div className="container mt-3 ml-auto card-body my-3 card col-md-6 row justify-content-center">
+        <div className="card-header my-2">Skills and Achievments</div>
+        <div>
+          <form>
+            <div className="form-group input-group my-3">
+              <label className="input-group-text" htmlFor="skills">Skills</label>
+              <input type="text" className="form-control" id="skills" placeholder="Enter Your Skill" />
+            </div>
+            <div className=" my-2">
+              <button type="submit" id="addskill" className="mx-4 btn btn-light">Add Skills</button>
+            </div>
+            <div className="form-group input-group my-3">
+              <label className="input-group-text" htmlFor="achievment">Achievments</label>
+              <input type="text" className="form-control" id="achievment" placeholder="Enter Your Achievments" />
+            </div>
+            <div className=" my-2">
+              <button type="submit" id="addachievment" className="mx-4 btn btn-light">Add Achievment</button>
+            </div>
+            <div className=" my-2">
+              <button type="submit" id="nextTowork" className="mx-2 btn btn-primary">Save and Next</button>
+            </div>
+          </form>
+        </div>
+      </div>
+
     </>
 
   )
